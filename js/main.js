2f6bc08f5f8f381e57da770faec18d8f329425e6
annafagocistka <anafagocistka@gmail.com>
2019-04-04 12:56:05 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,100 @@
+'use strict';
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#main-nav').addClass('changeColor');
+        }
+        if ($(this).scrollTop() < 50) {
+            $('#main-nav').removeClass('changeColor');
+        }
+
+        var outerHight = $("#main-nav").outerHeight();
+        var topMenuHeight = outerHight + 15;
+
+        $(".page-section").each(function (i) {
+            if ($(this).hasClass("contact")) {
+                topMenuHeight = outerHight + 150;
+            }
+            var scrollDistance = $(window).scrollTop() + topMenuHeight;
+            if ($(this).position().top <= scrollDistance && $(this).position().top + $(this).height() > scrollDistance) {
+                $('#main-nav li a').removeClass('activeMenu');
+                $('#main-nav li a').eq(i).addClass('activeMenu');
+
+            } 
+//            else {
+//                $('main-nav li a').removeClass('activeMenu');
+//            }
+        });
+
+        $.each(['.fadeDown', '.fadeUp'], function (i) {
+            var bottomOfObject = $(this).position().top + $(this).outerHeight();
+            var bottomOfWindow = $(window).scrollTop() + $(window).height();
+
+            if (bottomOfWindow > bottomOfObject) {
+                $(this).addClass('activate');
+            }
+        });
+    });
+});
+
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+        var target = $($(this).attr('href'));
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top - 30
+            }, 800);
+        }
+    });
+
+    $('a[href^="#"]').click(function () {
+        $('#main-nav').slideUp("fast", function () {
+            $(this).slideDown("slow");
+        });
+    });
+    $('.btn-header').click(function (event) {
+        var target = $('#contact');
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 800);
+    });
+
+//    $(".nav-item").click(function () {
+//        $(".nav-item").removeClass('activeMenu');
+//        $(this).addClass('activeMenu');
+//    });
+
+    $(".dropdown-li").on('click', function () {
+        $(".dropdown-content").css("display", "none");
+        console.log('dziala');
+    });
+    
+    $(function () {
+        var navMobile = $('.mobileHide');
+        $(document.body).on('click', function () {
+            navMobile.collapse('hide');
+        });
+    });
+
+    if (/iP(hone|od|ad)/.test(navigator.platform)) {
+        $("*").css({
+            "cursor": "pointer"
+        });
+    }
+
+    $(".language-select").click(function () {
+        i18next.changeLanguage($(this).attr('id'), function () {
+            $('.menu-lng').localize();
+            $('.header-lng').localize();
+            $('.aboutme').localize();
+            $('.skills').localize();
+            $('.contact').localize();
+        });
+    });
+
+    $(".english").html("english");
+    $(".deutsch").html("deutsch");
+    $(".polski").html("polski");
+});
